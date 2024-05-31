3bb9599c8e38d706cb76d0c0462d7e57ac61d4b7
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-05-30 17:32:37 -0700

    defines the columns for the category, product, tag and productTag tables

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,34 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
+const tag = require('./Tag');
+const product = require('./Product');
+
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+    prouct_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:product,
+        key:id,
+      },
+    },
+    tag_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model:tag,
+        key:id,
+      },
+    },
   },
   {
     sequelize,
