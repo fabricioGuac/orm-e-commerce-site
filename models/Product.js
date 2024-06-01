2fb25a11c36cc82a1ce882a976814fc61790c93a
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-05-31 20:59:38 -0700

    delves in the logic for getting, creating, updating and deleting categories

--- models/Product.js
+++ models/Product.js
@@ -36,7 +36,7 @@ Product.init(
         isNumeric:true,
       },
     },
-    Category_id:{
+    category_id:{
       type:DataTypes.INTEGER,
       references:{
         model:Category,
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,76 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  // Check if the id parameter contains only numeric characters
+  if (!/^\d+$/.test(req.params.id)) {
+    return res.status(400).json({ message: 'Invalid product ID.' });
+  }
+  try {
+    if(!tag){
+      res.status(404).json({message: 'Category not found'});
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  // Check if the id parameter contains only numeric characters
+  if (!/^\d+$/.test(req.params.id)) {
+    return res.status(400).json({ message: 'Invalid product ID.' });
+  }
+  try {
+    if(tag[0] === 0){
+      res.status(404).json({message: 'Category not found'});
+      return;
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  // Check if the id parameter contains only numeric characters
+  if (!/^\d+$/.test(req.params.id)) {
+    return res.status(400).json({ message: 'Invalid product ID.' });
+  }
+  try {
+    const delTag = await Tag.destroy({
+      where: {id: req.params.id},
+    });
+    if(!delTag){
+      res.status(404).json({message: 'Category not found'});
+      return;
+    }
+    res.status(200).json(delTag)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
